0e11c68d50e934ba883b98e07b3c57ead9e3dcd5
exepulgar <exepulgar-git@gmail.com>
2026-06-24 16:02:20 -0400

    Modificación de Puertos

--- diputado_profile_service/src/main/java/cl/patria/diputado_profile_service/service/ProfileService.java
+++ diputado_profile_service/src/main/java/cl/patria/diputado_profile_service/service/ProfileService.java
@@ -1,15 +1,14 @@
 package cl.patria.diputado_profile_service.service;
 
 import cl.patria.diputado_profile_service.model.ProfileEntity;
-import cl.patria.diputado_profile_service.model.dto.ProfileDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface ProfileService {
-    String uriDiputadosScraper = "http://patria-scrap-diputados:8081/api/v1/scrap/diputados";
-    String uriDistritosScraper = "http://patria-scrap-locations:8082/api/v1/scrap/distritos";
+    String uriDiputadosScraper = "http://patria-scrap-diputados:8080/api/v1/scrap/diputados";
+    String uriDistritosScraper = "http://patria-scrap-locations:8080/api/v1/scrap/distritos";
     List<ProfileEntity> getProfiles();
     Optional<ProfileEntity> getProfile(int id);
 
